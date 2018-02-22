11365df548c428e8752af7a473dd1ae5aff2ab4f
dgildeh <dgildeh@davidgildeh.com>
2018-02-22 00:55:54 -0800

    Incremented Python Module Number

--- python/setup.py
+++ python/setup.py
@@ -10,7 +10,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 setup(
   name = 'jmxquery',
   packages = ['jmxquery'],
-  version = '0.1.0',
+  version = '0.2.0',
   description = 'A JMX Interface for Python to Query runtime metrics in a JVM',
   long_description=long_description,
   author = 'David Gildeh',
