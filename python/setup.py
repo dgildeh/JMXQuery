75eb519e6e2d5bcd393bee910595651f90f6de67
David Gildeh <dgildeh@davidgildeh.com>
2019-11-14 22:56:54 -0800

    Fixed setup.py for PyPi changes

--- python/setup.py
+++ python/setup.py
@@ -3,10 +3,11 @@ from codecs import open
 from os import path
 
 """
-  Refer to http://peterdowns.com/posts/first-time-with-pypi.html for instructions on how to 
+  Refer to https://packaging.python.org/tutorials/packaging-projects/ for instructions on how to 
   push new versions of this module to PyPI. Use the following command to push new versions of the module:
   
-  python3 setup.py sdist upload -r pypi
+  python3 setup.py sdist bdist_wheel
+  twine upload dist/*
   
   Make sure you update the MANIFEST.in file if you change the name of the JAR file so its included in the 
   module when submitted
@@ -20,12 +21,13 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 setup(
   name = 'jmxquery',
   packages = ['jmxquery'],
-  version = '0.5.0',
+  version = '0.6.0',
   description = 'A JMX Interface for Python to Query runtime metrics in a JVM',
   long_description=long_description,
+  long_description_content_type="text/markdown",
   author = 'David Gildeh',
   author_email = 'david.gildeh@outlyer.com',
-  url = 'https://github.com/outlyerapp/jmxquery',
+  url = 'https://github.com/dgildeh/jmxquery',
   keywords = ['java', 'jmx', 'metrics', 'monitoring'],
   classifiers = [],
   include_package_data=True,
